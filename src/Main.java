faa45470f3670d5cb78545b8a18a493a4ea44ea5
PACKOJIbHUKOB <jordn-5@yandex.ru>
2023-06-14 15:21:24 +0300

    задание 1-2

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {   task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();}
+    private static void task1() {
+        System.out.println("задача  1");
+        for(int i =0; i<10;i=i+1){
+            System.out.println("цикл i "+i );}
+    }
+    private static void task2() {
+        System.out.println("задача 2");
+        for (int a = 10; a>0; a--){
+            System.out.println("цикл a "+a);}
+    }
+    private static void task3() {
+        System.out.println("задача 3");
+        for(int b = 0; b<17;b+=2){
+            System.out.println("цикл b "+b);}
+    }
+    private static void task4() {
+        System.out.println("задача 4");}
+
+    private static void task5() {
+        System.out.println("задача 5");}
+
+    private static void task6() {
+        System.out.println("задача 6");}
+
+    private static void task7() {
+        System.out.println("задача 7");}
+
+    private static void task8() {
+        System.out.println("задача 8");}
+}
